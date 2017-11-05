039d1d35b42a2e669b4cdd5885624d9b70a6fc23
dluthwy <dlutweiye@gmail.com>
2017-11-05 14:14:36 +0800

    mySMS first commit

---
+++ mySMS/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace mySMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSign = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(97, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "对方手机：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(97, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "发送内容：";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtPhone.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPhone.Location = new System.Drawing.Point(0, 0);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(243, 30);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtMsg.Location = new System.Drawing.Point(207, 140);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(271, 130);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSend.Location = new System.Drawing.Point(388, 310);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(90, 50);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(97, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "签    名：";
+            // 
+            // txtSign
+            // 
+            this.txtSign.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSign.Location = new System.Drawing.Point(207, 320);
+            this.txtSign.Name = "txtSign";
+            this.txtSign.Size = new System.Drawing.Size(146, 30);
+            this.txtSign.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(241, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtPhone);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(207, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 30);
+            this.panel1.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 403);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtSign);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mySMS";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSign;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ mySMS/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mySMS
+{
+    public partial class Form1 : Form
+    {
+        private string url = "http://utf8.api.smschinese.cn/?";
+        private string strUid = "Uid=";
+        private string strKey = "&key=12e8c5a290b27437b257";
+        private string strMob = "&smsMob=";
+        private string strContent = "&smsText=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetHtmlFromUrl(string url)
+        {
+            string strRet = null;
+            if (url == null || url.Trim().ToString() == "")
+            {
+                return strRet;
+            }
+            string targeturl = url.Trim().ToString();
+            try
+            {
+                HttpWebRequest hr = (HttpWebRequest)WebRequest.Create(targeturl);
+                hr.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)";
+                hr.Method = "GET";
+                hr.Timeout = 30 * 60 * 1000;
+                WebResponse hs = hr.GetResponse();
+                Stream sr = hs.GetResponseStream();
+                StreamReader ser = new StreamReader(sr, Encoding.Default);
+                strRet = ser.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                strRet = null;
+            }
+            return strRet;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(txtPhone.Text.ToString().Trim() != "" && txtMsg.Text.ToString().Trim() != "" && txtSign.Text.ToString().Trim() != "")
+            {
+                url = url + strUid + "dluthwy" + strKey + strMob + txtPhone.Text + strContent + txtMsg.Text + "【"+ txtSign.Text + "】";
+                string Result = GetHtmlFromUrl(url);
+                if (Result == "-1")
+                    MessageBox.Show("没有该用户账户");
+                else if (Result == "-2")
+                    MessageBox.Show("不是账户登陆密码");
+                else if (Result == "-21")
+                    MessageBox.Show("MD5接口密钥加密不正确");
+                else if (Result == "-3")
+                    MessageBox.Show("短信数量不足，请充值");
+                else if (Result == "-11")
+                    MessageBox.Show("该用户被禁用");
+                else if (Result == "-14")
+                    MessageBox.Show("短信内容出现非法字符");
+                else if (Result == "-4")
+                    MessageBox.Show("手机号格式不正确");
+                else if (Result == "-6")
+                    MessageBox.Show("IP限制");
+                else if (Convert.ToInt32(Result) > 0)
+                    MessageBox.Show("已成功发送" + Result + "条信息");
+            }
+            else
+            {
+                MessageBox.Show("发送格式不正确！");
+            }
+        }
+
+        int sendNum;
+        private void button1_Click(object sender, EventArgs e)//从Excel导入数据
+        {
+            try
+            {
+                sendNum = 0;
+                DataTable dt = getData().Tables[0];
+                Regex regex = new Regex(@"^[1][3-8]\d{9}$");
+                string allPhone = "";
+                foreach(DataRow row in dt.Rows)
+                {
+                    for(int i = 0;i < dt.Columns.Count; i ++)
+                    {
+                        if(regex.IsMatch(row[i].ToString()))
+                        {                   
+                            allPhone += row[i].ToString() + ",";
+                            sendNum++;
+                        }
+                    }
+                }
+                MessageBox.Show("导入" + sendNum.ToString() + "位联系人");
+                txtPhone.Text = allPhone;
+            }
+            catch (Exception ex)
+            {
+                return;
+            }         
+        }
+
+        public DataSet getData()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel(*.xlsx)|*.xlsx|Excel(*.xls)|*.xls";
+            file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            file.Multiselect = false;
+            if (file.ShowDialog() == DialogResult.Cancel)
+                return null;
+            var path = file.FileName;
+            string fileSuffix = System.IO.Path.GetExtension(path);
+            if (string.IsNullOrEmpty(fileSuffix))
+                return null;
+            using (DataSet ds = new DataSet())
+            {
+                string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + path + ";Extended Properties='Excel 12.0; HDR=NO; IMEX=1'";
+                OleDbConnection conn = new OleDbConnection(strConn);
+                conn.Open();
+                string strExcel = "";
+                OleDbDataAdapter myCommand = null;
+                strExcel = "select * from [sheet1$]";
+                myCommand = new OleDbDataAdapter(strExcel, strConn);
+                myCommand.Fill(ds, "table1");
+                return ds;
+            }
+        }
+    }
+}
